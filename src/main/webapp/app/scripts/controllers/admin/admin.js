dad54adb44de6981205aea56bbb341b623a5f117
droodev <droodev@gmail.com>
2015-07-18 10:56:34 +0200

    Added some flow connected with adding new Journal

--- src/main/webapp/app/scripts/controllers/admin/admin.js
+++ src/main/webapp/app/scripts/controllers/admin/admin.js
@@ -1,12 +1,18 @@
 'use strict';
 
 angular.module('autorApp')
-  .controller('AdminController', function ($scope, Restangular) {
+  .controller('AdminController', function ($scope, $location, Restangular) {
     $scope.newJournal = {};
     $scope.newJournal.logo = "HJJSDJJ";
 
     $scope.addJournal = function() {
-      Restangular.all('journals').customPOST({},"",$scope.newJournal,{});
+      Restangular.all('journals').customPOST({},"",$scope.newJournal,{}).then(function(res){
+        $location.path('/journals');
+        $route.reload();
+      }, function(res){
+        alert(res.status)
+      });
+
     }
 
   });
