c147776fa4f94dbf24666c6951f66279bd10e9a0
droodev <droodev@gmail.com>
2015-09-01 10:11:38 +0200

    Some minor fixes and refactoring

--- src/main/webapp/app/scripts/controllers/papers/new-paper.js
+++ src/main/webapp/app/scripts/controllers/papers/new-paper.js
@@ -4,7 +4,7 @@ angular.module('autorApp')
   .controller('NewPaperController', function ($scope, $location, $routeParams, Restangular) {
 
     $scope.newPaper = {authors: [{}]};
-    $scope.journalID = $routeParams.journals;
+    $scope.journalID = $routeParams.id;
     $scope.newPaper.authors[0].corresponding = true;
 
     Restangular.one('journals', $scope.journalID).get().then(
@@ -26,9 +26,13 @@ angular.module('autorApp')
       if ($scope.newpaperForm.$valid) {
         var auths = $scope.newPaper.authors
         for (var ind in auths) {
-          if (auths[ind].corresponding === "true") {
+          if(auths[ind].corresponding === "true"){
             auths[ind].correspondencyData = $scope.contactData;
+            auths.unshift(auths.splice(ind, 1)[0]);
+            break;
           }
+        }
+        for (var ind in auths) {
           delete auths[ind].corresponding
         }
         Restangular.all('papers').customPOST($scope.newPaper, "", {journalID: $scope.journalID}, {}).then(function (res) {
@@ -38,5 +42,4 @@ angular.module('autorApp')
         });
       }
     }
-
   });
