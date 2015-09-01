6138ad7b154634cc19d2682cc4dc3b21a296f248
droodev <droodev@gmail.com>
2015-09-01 17:33:56 +0200

    Cleaning

--- src/main/webapp/app/scripts/controllers/papers/new-paper.js
+++ src/main/webapp/app/scripts/controllers/papers/new-paper.js
@@ -40,7 +40,6 @@ angular.module('autorApp')
 
         $http.get("https://api.ipify.org?format=json").then(function (response) {
           $scope.newPaper.IPAddress = response.data.ip
-          alert($scope.newPaper.IPAddress)
           Restangular.all('papers').customPOST($scope.newPaper, "", {journalID: $scope.journalID}, {}).then(function (res) {
             $location.path('/confirmation');
           }, function (res) {
